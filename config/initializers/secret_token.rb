61e9c9c5cd9022d4db2c042d17dec9ff66ead8af
Jeff Casimir <jeff@casimircreative.com>
2011-04-17 15:14:23 -0400

    Initial project generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JSContact::Application.config.secret_token = '61b4594c471a1b46919872a13508f659a943a51e42b8ee02cdb30d707d287e5eb64e184419326e31e55c4fc879ba38202bb9b74e5f2a606c308e749d0ca0edc4'
