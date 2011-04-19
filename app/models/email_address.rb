e84805988dbb58ae4e8dd2680035022a3ff3cc31
Jeff Casimir <jeff@casimircreative.com>
2011-04-19 17:39:09 -0400

    Basic relationships for Email Addresses

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -2,4 +2,6 @@ class EmailAddress < ActiveRecord::Base
   attr_accessible :address, :person_id
   
   belongs_to :person
+  
+  validates_presence_of :address, :person
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,22 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe EmailAddress do
+  before(:each) do
+    @person = Person.create(:first_name => "John", :last_name => "Doe")
+    @email_address = @person.email_addresses.new(:address => "john@doe.com")
+  end
+  
   it "should be valid" do
-    EmailAddress.new.should be_valid
+    @email_address.should be_valid
+  end
+  
+  it "should not be valid without an address" do
+    @email_address.address = nil
+    @email_address.should_not be_valid
+  end
+  
+  it "should not be valid without a person" do
+    @email_address.person_id = nil
+    @email_address.should_not be_valid
   end
 end
